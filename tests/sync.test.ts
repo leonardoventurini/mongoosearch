449917c5b98211490f9e4bbffc429fe1b65b2682
Leonardo Venturini <leovenbag@gmail.com>
2022-04-20 20:07:59 -0400

    improve sync tests

--- tests/sync.test.ts
+++ tests/sync.test.ts
@@ -1,22 +1,15 @@
 import { expect } from 'chai'
-import { TestDatabase } from './utils/test-database'
 import { SnakeCollection } from './utils/collections/snake-collection'
 import faker from 'faker'
 import { clearIndexes } from './utils/clear-indexes'
-
-const firstSnake = faker.animal.snake()
+import { ElasticsearchClient } from './utils/elasticsearch-client'
 
 describe('Synchronization', () => {
   beforeEach(async () => {
-    await clearIndexes()
     await SnakeCollection.deleteMany({})
-
-    await SnakeCollection.create({
-      sample: firstSnake,
-    })
+    await clearIndexes()
 
     await SnakeCollection.esCreateMapping()
-    await SnakeCollection.esSync()
   })
 
   afterEach(async () => {
@@ -24,36 +17,52 @@ describe('Synchronization', () => {
     await clearIndexes()
   })
 
-  afterEach(async () => {
-    await TestDatabase.clearDatabase()
-  })
-
-  it('should synchronize', async () => {
-    const result = await SnakeCollection.esSync()
-
-    // @ts-ignore
-    expect(result?._shards?.successful).to.be.greaterThan(0)
-  })
-
-  it('should search', async () => {
-    await clearIndexes()
-
-    await SnakeCollection.create({ sample: 'Sample 2' })
+  it('should do a partial sync', async () => {
+    await SnakeCollection.create({ sample: 'Bogus' })
+    await SnakeCollection.create({ sample: 'Sample' })
 
     await SnakeCollection.esSync({ sample: /sample/i })
 
-    const result = await SnakeCollection.esSearch({
+    const result = await SnakeCollection.esSearch(
+      {
+        query: {
+          match_all: {},
+        },
+      },
+      { hydrate: { docsOnly: true } },
+    )
+
+    expect(result).to.have.length.greaterThan(0)
+
+    expect(result.map(({ sample }) => /sample/i.test(sample)).every(Boolean)).to
+      .be.true
+  })
+
+  it('should sync a large dataset successfully', async () => {
+    const snakeDataset = [...new Array(1000)].map(() => faker.animal.snake())
+
+    for (const snake of snakeDataset) {
+      await SnakeCollection.create({ sample: snake })
+    }
+
+    const result = await SnakeCollection.esSync()
+
+    expect(result)
+      .to.have.property('_shards')
+      .that.has.property('successful')
+      .that.is.greaterThan(0)
+
+    const docs = await ElasticsearchClient.search({
+      index: 'test_snakes',
       query: {
         match_all: {},
       },
     })
 
-    const { hits } = result?.hits ?? {}
-
-    expect(hits).to.have.length.greaterThan(0)
-
-    expect(
-      hits.map(({ _source }) => /sample/i.test(_source.sample)).every(Boolean),
-    ).to.be.true
+    expect(docs).to.containSubset({
+      hits: {
+        total: { value: snakeDataset.length },
+      },
+    })
   })
 })
